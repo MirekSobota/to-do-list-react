b9da5980806dab6be78676666b2f9a758c6263d1
MirekSobota <miroslaw.sobota@gmail.com>
2023-05-23 20:48:27 +0200

    Adding styled-components to Container

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,16 +1,11 @@
-
 import { Functions__Buttons, Button } from "./styled";
 const Buttons = ({ tasks, hideDone, toggleHideDone, setAllDone }) =>
   tasks.length > 0 && (
     <Functions__Buttons>
-      <Button
-       onClick={toggleHideDone} >
+      <Button onClick={toggleHideDone}>
         {!hideDone ? "Hide" : "Show"} done tasks
       </Button>
-      <Button
-        onClick={setAllDone}
-        disabled={tasks.every(({ done }) => done)}
-      >
+      <Button onClick={setAllDone} disabled={tasks.every(({ done }) => done)}>
         Finish all tasks
       </Button>
     </Functions__Buttons>
