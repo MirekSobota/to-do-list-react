99847c1192a5869abf6a446d38169e72e13fdbc6
MirekSobota <miroslaw.sobota@gmail.com>
2023-05-30 20:34:36 +0200

    Creating custom hook useTasks for all functions

--- src/App.js
+++ src/App.js
@@ -4,81 +4,48 @@ import Buttons from "./Buttons";
 import Section from "./Section";
 import Header from "./Header";
 import Container from "./Container";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { ThemeProvider } from "styled-components";
-import {theme} from "./Theme/theme";
-
-const getInitialTasks = () => {
-  const tasksFromLocalStorage = localStorage.getItem("tasks");
-  return tasksFromLocalStorage ? JSON.parse(tasksFromLocalStorage) : [];
-};
+import { theme } from "./Theme/theme";
+import { useTasks } from "./useTasks";
 
 function App() {
   const [hideDone, setHideDone] = useState(false);
-  const [tasks, setTasks] = useState(getInitialTasks);
-
-  useEffect(() => {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-  }, [tasks]);
 
   const toggleHideDone = () => {
     setHideDone((hideDone) => !hideDone);
   };
 
-  const removeTask = (id) => {
-    setTasks((tasks) => tasks.filter((task) => task.id !== id));
-  };
-
-  const toggleTaskDone = (id) => {
-    setTasks((tasks) =>
-      tasks.map((task) =>
-        task.id === id ? { ...task, done: !task.done } : task
-      )
-    );
-  };
-
-  const setAllDone = () => {
-    setTasks((tasks) => tasks.map((task) => ({ ...task, done: true })));
-  };
-
-  const addNewTask = (newTaskContent) => {
-    setTasks((tasks) => [
-      ...tasks,
-      {
-        content: newTaskContent,
-        done: false,
-        id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1,
-      },
-    ]);
-  };
+  const { tasks, removeTask, toggleTaskDone, setAllDone, addNewTask } =
+    useTasks();
 
   return (
-<ThemeProvider theme={theme}>
-<Container>
-      <Header title="Tasks list" />
-      <Section title="Add new task" body={<Form addNewTask={addNewTask} />} />
+    <ThemeProvider theme={theme}>
+      <Container>
+        <Header title="Tasks list" />
+        <Section title="Add new task" body={<Form addNewTask={addNewTask} />} />
 
-      <Section
-        title="A list of tasks"
-        extraHeaderContent={
-          <Buttons
-            tasks={tasks}
-            hideDone={hideDone}
-            toggleHideDone={toggleHideDone}
-            setAllDone={setAllDone}
-          />
-        }
-        body={
-          <Tasks
-            tasks={tasks}
-            hideDone={hideDone}
-            removeTask={removeTask}
-            toggleTaskDone={toggleTaskDone}
-          />
-        }
-      />
-    </Container>
-</ThemeProvider>
+        <Section
+          title="A list of tasks"
+          extraHeaderContent={
+            <Buttons
+              tasks={tasks}
+              hideDone={hideDone}
+              toggleHideDone={toggleHideDone}
+              setAllDone={setAllDone}
+            />
+          }
+          body={
+            <Tasks
+              tasks={tasks}
+              hideDone={hideDone}
+              removeTask={removeTask}
+              toggleTaskDone={toggleTaskDone}
+            />
+          }
+        />
+      </Container>
+    </ThemeProvider>
   );
 }
 
