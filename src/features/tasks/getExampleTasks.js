dca35ec9904df5826a14b1134b48a25d81fc4c60
MirekSobota <miroslaw.sobota@gmail.com>
2023-06-22 22:16:13 +0200

    Changed fetch url

--- src/features/tasks/getExampleTasks.js
+++ src/features/tasks/getExampleTasks.js
@@ -1,5 +1,5 @@
 export const getExampleTasks = async () => {
-    const response = await fetch("todo-list-react/exampleTasks.json");
+    const response = await fetch("public/exampleTasks.json");
 
     if(!response.ok) {
         new Error(response.statusText);
