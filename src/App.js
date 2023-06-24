2fcf454c1d7850f540345cf411dc1c5c274cf237
MirekSobota <miroslaw.sobota@gmail.com>
2023-06-24 20:45:06 +0200

    Working on Nav, Tasks and Author

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import {HashRouter, Switch, Route, Link}  from "react-router-dom"
+
 import { Tasks } from "./features/tasks/Tasks";
 
-const App = () => <Tasks />;
+const App = () => 
+
+<HashRouter>
+<nav>
+    <ul>
+        <li>
+        <Link to ="/tasks">TASKS</Link>
+        </li>
+    </ul>
+</nav>
+<Switch>
+<Route path="/tasks">
+    <Tasks />
+</Route>
+</Switch>
+</HashRouter>
+
 
 export { App };
